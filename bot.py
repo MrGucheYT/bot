369d42a8fbe1248e22044b3f9adce82558963e7c
MrGucheYT <v.smyshlyaev1410@yandex.ru>
2025-06-15 10:12:00 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 import telebot
+import requests
+import os
+import random
 from config import token    
 from logic import gen_pass
 from telebot.types import (
@@ -43,5 +46,30 @@ def mini_app(message):
 @bot.message_handler(content_types=["wed_app_data"])
 def web_app(message):
     bot.reply_to(message, f'Your message is "{message.web_app_data.data}"')
+
+@bot.message_handler(commands=['mem'])
+def send_mem(message):
+    img_name=random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:  
+        bot.send_photo(message.chat.id, f)  
+
+@bot.message_handler(commands=['animals'])
+def send_animals(message):
+    img_name=random.choice(os.listdir('animals'))
+    with open(f'animals/{img_name}', 'rb') as f:  
+        bot.send_photo(message.chat.id, f)  
+
+def get_duck_image_url():    
+        url = 'https://random-d.uk/api/random'
+        res = requests.get(url)
+        data = res.json()
+        return data['url']
+
+@bot.message_handler(commands=['duck'])
+def duck(message):
+    '''По команде duck вызывает функцию get_duck_image_url и отправляет URL изображения утки'''
+    image_url = get_duck_image_url()
+    bot.reply_to(message, image_url)
+
 
 bot.infinity_polling()
